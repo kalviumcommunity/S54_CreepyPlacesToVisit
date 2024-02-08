34002917e57c644e3b2f13a148fd4ccc7c7bebe2
manpreetsingh04m <ms2404mk@gmail.com>
2024-02-08 16:05:56 +0530

    Change in database

--- Backend/init/index.js
+++ Backend/init/index.js
@@ -22,6 +22,7 @@ main()
 const data = [
   new Places({
     title: "Abandoned Asylum",
+    location: "Outskirts of Towns",
     image: "https://t4.ftcdn.net/jpg/05/62/81/65/240_F_562816532_9cpzxBULerlNQ0UeuHHfWnTAd64PBf1z.jpg",
     description: "An eerie structure on the outskirts of towns, this abandoned asylum stands as a haunting reminder of past traumas. Its crumbling walls echo the stories of those who once sought refuge within.",
     user: "sampleuser123",
@@ -29,6 +30,7 @@ const data = [
   }),
   new Places({
     title: "Haunted Mansion",
+    location: "America",
     image: "https://d3f7q2msm2165u.cloudfront.net/aaa-content/user/files/2023/Blog/Sonia/Screen%20Shot%202023-10-23%20at%201.36.30%20PM-min.png",
     description: "A decrepit mansion shrouded in mystery, its dark corridors and shadowy rooms hold secrets of a past long forgotten. Ghostly apparitions and strange phenomena are said to haunt its halls.",
     image: "https://d3f7q2msm2165u.cloudfront.net/aaa-content/user/files/2023/Blog/Sonia/Screen%20Shot%202023-10-23%20at%201.36.30%20PM-min.png",
@@ -37,6 +39,7 @@ const data = [
   }),
   new Places({
     title: "Island of the Dolls",
+    location: "Mexico",
     image: "https://nypost.com/wp-content/uploads/sites/2/2021/10/island-of-the-dolls-main.jpg?quality=75&strip=all",
     description: "Located in the heart of Mexico, the Island of the Dolls is adorned with hundreds of dolls hanging from trees. Legend has it that the island is haunted by the spirit of a young girl who drowned nearby.",
     user: "sampleuser125",
@@ -44,6 +47,7 @@ const data = [
   }),
   new Places({
     title: "Aokigahara Forest",
+    location: "Japan",
     image: "https://www.verdict.co.uk/wp-content/uploads/2018/01/shutterstock_722719306-scaled.jpg",
     description: "Also known as the Suicide Forest, Aokigahara is a dense forest at the base of Mount Fuji in Japan. It has gained notoriety as a site for suicides, with many tragic stories associated with its eerie atmosphere.",
     user: "sampleuser126",
@@ -51,6 +55,7 @@ const data = [
   }),
   new Places({
     title: "Poveglia Island",
+    location: "Venice, Italy",
     image: "https://staticimg.amarujala.com/assets/images/2019/08/20/750x506/poveglia-island-italy_1566312692.jpeg",
     description: "Located off the coast of Venice, Italy, Poveglia Island has a dark history as a quarantine station and later as an asylum. It is rumored to be haunted by the tortured souls of its past inhabitants.",
     user: "sampleuser127",
@@ -58,6 +63,7 @@ const data = [
   }),
   new Places({
     title: "Chernobyl Exclusion Zone",
+    location: "Ukraine",
     image: "https://media.nbcchicago.com/2021/04/AP_21114459770757-3.jpg?quality=85&strip=all&fit=5464%2C3640",
     description: "The site of the infamous Chernobyl nuclear disaster, the Exclusion Zone is a desolate and eerie place frozen in time. Abandoned buildings, rusting machinery, and high radiation levels characterize this haunting landscape.",
     user: "sampleuser128",
@@ -65,6 +71,7 @@ const data = [
   }),
   new Places({
     title: "Sedlec Ossuary",
+    location: "Kutná Hora, Czech Republic",
     image: "https://sedlecossuary.com/wp-content/uploads/2022/09/Sedlec_Ossuary_chandelier.jpg",
     description: "Located beneath the Cemetery Church of All Saints in Kutná Hora, Czech Republic, the Sedlec Ossuary is a chapel adorned with human bones. Its macabre decorations include a chandelier made entirely of bones.",
     user: "sampleuser129",
@@ -72,6 +79,7 @@ const data = [
   }),
   new Places({
     title: "Pripyat Ghost Town",
+    location: "Pripyat, Ukraine",
     image: "https://i.dailymail.co.uk/1s/2021/10/27/09/49690939-0-image-a-3_1635321719830.jpg",
     description: "Once a bustling city near the Chernobyl nuclear power plant, Pripyat was evacuated and abandoned after the 1986 disaster. Today, it stands as a haunting reminder of the consequences of nuclear catastrophe.",
     user: "sampleuser130",
@@ -79,6 +87,7 @@ const data = [
   }),
   new Places({
     title: "Bodie Ghost Town",
+    location: "California, USA",
     image: "https://images.squarespace-cdn.com/content/v1/5a5986b2cf81e095e172ce87/6844cd21-bb78-4515-af6a-b7a949425215/flyingdawnmarie-bodie-ghost-town-11.jpg",
     description: "Bodie is a well-preserved ghost town in California, USA, frozen in time from the late 19th century gold rush era. Its abandoned buildings and eerie atmosphere attract visitors interested in its haunted history.",
     user: "sampleuser131",
@@ -86,6 +95,7 @@ const data = [
   }),
   new Places({
     title: "The Catacombs of Paris",
+    location: "Paris, France",
     image: "https://i0.wp.com/bookme.tours/wp-content/uploads/2023/05/What-Is-Inside-The-Catacombs-of-Paris.jpg?fit=1506%2C1000&ssl=1",
     description: "Beneath the streets of Paris lie the Catacombs, a vast network of tunnels lined with the bones of millions of people. This macabre underground ossuary has fascinated visitors for centuries.",
     user: "sampleuser132",
@@ -93,14 +103,14 @@ const data = [
   }),
 ];
 
-// Places.insertMany(data)
-//   .then((docs)=>{
-//     console.log('====================================');
-//     console.log("Data Pushed");
-//     console.log('====================================');
-//   })
-//   .catch((err)=>{
-//     console.log('====================================');
-//     console.log(err);
-//     console.log('====================================');
-//   })
+Places.insertMany(data)
+  .then((docs)=>{
+    console.log('====================================');
+    console.log("Data Pushed");
+    console.log('====================================');
+  })
+  .catch((err)=>{
+    console.log('====================================');
+    console.log(err);
+    console.log('====================================');
+  })
